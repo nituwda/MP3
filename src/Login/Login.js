f7c048141cdf24f295a290030920b084705fdc9c
Hans Nituda <hpnituda@gmail.com>
2022-05-07 03:20:55 +0800

    validation with design + can toggle between login and register

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -9,43 +9,46 @@ export default function LandingMessage() {
     password: "",
   });
 
-  const [signUp, setSignUp] = useState(false)
+  const [signUp, setSignUp] = useState(false);
   const [valid, setValid] = useState(false);
+  const [enter, setEnter] = useState(false);
 
+  const reg = () => {
+    setEnter(true);
+  };
 
-  // const signup = () => {
-  //   setRegister(true);
-  // };
-
-  // const login = () => {
-  //   setRegister(false);
-  // };
+  const afterReg = () => {
+    setEnter(false);
+  };
 
   const handleFirstNameChange = (e) => {
-    setRegister({...register, firstName:e.target.value});
-  }
+    setRegister({ ...register, firstName: e.target.value });
+  };
 
   const handleLastNameChange = (e) => {
-    setRegister({...register, lastName:e.target.value});
-  }
+    setRegister({ ...register, lastName: e.target.value });
+  };
 
   const handleEmailChange = (e) => {
-    setRegister({...register, email:e.target.value});
-  }
+    setRegister({ ...register, email: e.target.value });
+  };
 
   const handlePasswordChange = (e) => {
-    setRegister({...register, password:e.target.value});
-  }
+    setRegister({ ...register, password: e.target.value });
+  };
 
   const handleSignUpChange = (e) => {
     e.preventDefault();
-    if (register.firstName && register.lastName && register.email && register.password) {
+    if (
+      register.firstName &&
+      register.lastName &&
+      register.email &&
+      register.password
+    ) {
       setValid(true);
     }
     setSignUp(true);
-  }
-
-
+  };
 
   // const handleEmail = (e) => {
   //   setRegister({...register, email:e.target.value});
@@ -53,73 +56,106 @@ export default function LandingMessage() {
 
   return (
     <div className="container">
-      {!register ? (
+      {!register || enter ? (
         <div>
-          <h1> Welcome </h1>
-          <p> Welcome back! We missed you... </p>
-          <p>Please login to your account now. </p>
+          <div>
+            <h1> Welcome </h1>
+            <p> Welcome back! We missed you... </p>
+            <p> Please login to your account now. </p>
 
-          <form className="login-form">
-            <input type="text" placeholder="Email" />
-            
-            <input type="Password" placeholder="Password" />
-            
-            <button type="submit" className="log-in">
-              
-              log in
+            <form className="login-form">
+              <input type="text" placeholder="Email" />
+
+              <input type="Password" placeholder="Password" />
+
+              <button type="submit" className="log-in">
+                log in
+              </button>
+            </form>
+
+            <hr />
+
+            <p> You don't have an account yet? </p>
+            <button type="submit" className="sign-up" onClick={afterReg}>
+              register
             </button>
-          </form>
-
-          <hr />
-
-          <p> You don't have an account yet? </p>
-          <button type="submit" className="sign-up" onClick={signUp}>
-            register
-          </button>
+          </div>
         </div>
-
       ) : (
         <div>
           {/* register */}
-          {signUp && valid ? <div className="success-message"> Registration Successful </div>: null } 
+          {signUp && valid ? (
+            <div className="success-message">
+              Registration Successful! Now you can login.
+            </div>
+          ) : null}
           <h1> Welcome </h1>
           <p> You don't have an account yet? </p>
           <p> Let's create now! </p>
           <form className="reg-form" onSubmit={handleSignUpChange}>
-            <input type="text" placeholder="First Name" value={register.firstName} 
-            onChange={handleFirstNameChange}
-            disabled={valid}/>
-            {signUp && !register.firstName? <p id="first-name-error"> Please enter your first name</p>:null}
-            
-            
-            <input type="text" placeholder="Last Name" value={register.lastName}
-            onChange={handleLastNameChange}
-            disabled={valid}/>
-            {signUp && !register.lastName? <p id="last-name-error"> Please enter your last name</p>:null}
-            
-            
-            <input type="email" placeholder="Email" className="input" value={register.email}
-            onChange={handleEmailChange}
-            disabled={valid} />
-            {signUp && !register.email? <p id="email-error"> Please enter your email</p>:null}
-            
-            
-            <input type="Password" placeholder="Password" className="input" value={register.password}
-            onChange={handlePasswordChange}
-            disabled={valid} />
+            <input
+              type="text"
+              placeholder="First Name"
+              value={register.firstName}
+              onChange={handleFirstNameChange}
+              disabled={valid}
+            />
+            {signUp && !register.firstName ? (
+              <p id="first-name-error" className="error">
+                Please enter your first name
+              </p>
+            ) : null}
+
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={register.lastName}
+              onChange={handleLastNameChange}
+              disabled={valid}
+            />
+            {signUp && !register.lastName ? (
+              <p id="last-name-error" className="error">
+                Please enter your last name
+              </p>
+            ) : null}
+
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              value={register.email}
+              onChange={handleEmailChange}
+              disabled={valid}
+            />
+            {signUp && !register.email ? (
+              <p id="email-error" className="error">
+                Please enter your email
+              </p>
+            ) : null}
+
+            <input
+              type="Password"
+              placeholder="Password"
+              className="input"
+              value={register.password}
+              onChange={handlePasswordChange}
+              disabled={valid}
+            />
             <br />
-            {signUp && !register.password? <p id="password-error"> Please enter your password</p> : null}
+            {signUp && !register.password ? (
+              <p id="password-error" className="error">
+                Please enter your password
+              </p>
+            ) : null}
             {/* {signUp && (register.password <4) <p id="password-error"> Password must be more than 4 characters </p>: null} */}
-            
-            
-            
+
             <button type="submit" className="sign-up">
               register
             </button>
           </form>
           <hr />
-          <p> You don't have an account yet? </p>
-          <button type="submit" className="log-in" onClick={signUp}>
+          <p> Already have an account? </p>
+          <button type="submit" className="log-in" onClick={reg}>
             log in
           </button>
         </div>
